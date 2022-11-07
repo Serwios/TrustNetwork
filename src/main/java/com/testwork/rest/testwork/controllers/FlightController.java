c7b1ac89bbf13af1d04f863f2f52295d04428970
Serwio YK <kucelas@ukr.net>
2022-11-08 00:04:14 +0200

    Init

---
+++ src/main/java/com/testwork/rest/testwork/controllers/FlightController.java
@@ -0,0 +1,75 @@
+package com.testwork.rest.testwork.controllers;
+
+import com.testwork.rest.testwork.entities.Flight;
+import com.testwork.rest.testwork.services.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/flight")
+public class FlightController {
+    @Autowired
+    private FlightService flightService;
+
+    @GetMapping()
+    public List<Flight> getAllFlights() {
+        return flightService.getAll();
+    }
+
+    @PostMapping("/create")
+    public Flight createFlight(@RequestBody Flight flight) {
+        return flightService.create(flight);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Flight deleteId(@PathVariable Long id) {
+        return flightService.deleteById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Flight updateId(@PathVariable Long id,
+                           @RequestBody Flight flight) {
+
+        return flightService.update(id, flight);
+    }
+
+    @GetMapping("/{id}")
+    public Flight getFlightById(@PathVariable Long id) {
+        return flightService.getById(id);
+    }
+
+    @GetMapping("/get/{status}/{airCompanyId}")
+    public List<Flight> getAllByStatus(@PathVariable String status, @PathVariable Long airCompanyId) {
+        return flightService.getAllByStatusAndAirCompany(status, airCompanyId);
+    }
+
+    @PutMapping("/changeStatus/{flightId}/{status}")
+    public Flight changeStatus(@PathVariable Long flightId, @PathVariable String status) {
+        Flight flight = flightService.getById(flightId);
+        flight.setFlightStatus(status);
+
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = dateFormat.format(date);
+
+        switch (status) {
+            case "DELAYED":
+                flight.setDelayStartedAt(strDate);
+                break;
+            case "ACTIVE":
+                flight.setCreatedAt(strDate);
+                break;
+            case "COMPLETED":
+                flight.setEndedAt(strDate);
+                break;
+        }
+
+        return flightService.save(flight);
+    }
+}
